903c1057e0aa8ad9780839eedce0e3e5b97b1692
Olda Salek <mzk@mozektevidi.net>
2019-01-31 08:47:44 +0100

    tutorial 04

--- polls/views.py
+++ polls/views.py
@@ -3,26 +3,28 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
+from django.shortcuts import get_object_or_404, render
 from .models import Question, Choice
 from django.template import loader
+from django.views import generic
 
-def index(request):
-    latestQuestionList = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latestQuestionList])
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latestQuestionList,
-    }
-    return HttpResponse(template.render(context, request))
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -41,8 +43,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-def index2(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
